32888fab052635026ae5198d8c47ea07fd957867
Darvin <darvincitech@gmail.com>
2021-02-21 18:58:43 +0800

    Update MainActivity.java

--- app/src/main/java/com/darvin/security/MainActivity.java
+++ app/src/main/java/com/darvin/security/MainActivity.java
@@ -1,20 +1,7 @@
 package com.darvin.security;
 
-import android.app.ActivityManager;
-import android.content.Context;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.security.SecureRandom;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
